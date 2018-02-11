1c4b1adec9b5f159cef358dc02e8f0fe0b78e7b0
sungjkim34 <sungjkim34@gmail.com>
2018-02-11 05:23:20 -0500

    add showenrolled option

--- routes/account.js
+++ routes/account.js
@@ -18,7 +18,7 @@ module.exports = function(app, con){
             firstName: req.body.firstName.charAt(0).toUpperCase() + req.body.firstName.slice(1),
             lastName: req.body.lastName.charAt(0).toUpperCase() + req.body.lastName.slice(1)
         }
-        var sql = 'INSERT INTO accounts (username, password, email, firstName, lastName) VALUES (\'' + user.username + '\', \'' + user.password + '\', \'' + user.email + '\', \'' + user.firstName + '\', \'' + user.lastName + '\')';
+        var sql = 'INSERT INTO accounts (username, password, email, firstName, lastName, showEnrolled) VALUES (\'' + user.username + '\', \'' + user.password + '\', \'' + user.email + '\', \'' + user.firstName + '\', \'' + user.lastName + '\', \'' + false + '\')';
         con.query(sql, (err, result) => {
             if(err) res.send(err);
             res.send(result);
@@ -54,11 +54,20 @@ module.exports = function(app, con){
 
     app.get('/getAccount/:accountId', function(req, res){
         var accountId = req.params.accountId;
-        var sql = 'SELECT username, email, firstName, lastName, dob, major FROM accounts WHERE id = ' + accountId;
+        var sql = 'SELECT username, email, firstName, lastName, dob, major,  FROM accounts WHERE id = ' + accountId;
     });
 
     app.get('/getAccountCount', function(req, res) {
         var sql = 'SELECT COUNT(*) FROM accounts';
+        con.query(sql, (err, result, fields) => {
+            if(err) res.send(err);
+            res.send(result);
+        });
+    });
+
+    app.get('/toggleShowEnroll/:accountId', function(req, res){
+        var accountId = req.params.accountId;
+        var sql = 'UPDATE accounts SET showEnrolled = !showEnrolled WHERE id = ' + accountId;
         con.query(sql, (err, result, fields) => {
             if(err) res.send(err);
             res.send(result);
