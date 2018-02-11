befe873993b9f00159c780148c34de80400515a8
sungjkim34 <sungjkim34@gmail.com>
2018-02-10 20:55:19 -0500

    changes to enrollment

--- routes/enrollment.js
+++ routes/enrollment.js
@@ -8,7 +8,7 @@ module.exports = function(app, con, moment){
         });
     });
 
-    app.post('/registerClass', function(req, res) {
+    app.post('/enrollClass', function(req, res) {
         var studentId = req.body.studentId;
         var classId = req.body.classId;
         var sql = 'INSERT INTO enrollments (studentId, classId) VALUES (\'' + studentId + '\', \'' + classId + '\')';
@@ -19,15 +19,34 @@ module.exports = function(app, con, moment){
     });
 
     app.post('/dropClass', function(req, res) {
-        var studentId = req.body.studentId;
+        var enrollmentId = req.body.enrollmentId;
     
-        var sql = 'DELETE FROM enrollments WHERE id = ' + studentId;
+        var sql = 'DELETE FROM enrollments WHERE id = ' + enrollmentId;
         con.query(sql, (err, result) => {
             if(err) res.send(err);
             res.send(result);
         });
     });
 
+    app.post('/getStudentEnrollment/:studentId', function(req, res) {
+        var studentId = req.params.studentId;
+
+        var sql = 'SELECT * FROM enrollments WHERE studentId = ' + studentId;
+        con.query(sql, (err, result) => {
+            if(err) res.send(err);
+            res.send(result);
+        });
+    });
+
+    app.post('/getStudentEnrollmentDetailed/:studentId', function(req, res) {
+        var studentId = req.params.studentId;
+
+        var sql = 'SELECT enrollment.id as enrollmentId, class.id as classId, studentId, classId, name, courseName, firstName, lastName, courseId FROM enrollments as enrollment INNER JOIN classes as class ON enrollment.classId = class.id INNER JOIN professors as professor on class.professorId = professor.id WHERE studentId = ' + studentId;
+        con.query(sql, (err, result) => {
+            if(err) res.send(err);
+            res.send(result);
+        });
+    });
 
 
 }
